aaa2925592c2540d03b255168b4a0fb25b14cc51
StanleyKioko <mwangestanley02@gmail.com>
2024-08-11 18:26:34 +0300

    Dropdown Mwenu

--- Static/scripts.js
+++ Static/scripts.js
@@ -1,15 +1,8 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function() {
     const menuToggle = document.querySelector('.menu-toggle');
-    const navLinks = document.querySelector('nav ul');
+    const navMenu = document.querySelector('nav ul');
 
     menuToggle.addEventListener('click', function() {
-        navLinks.classList.toggle('active');
-    });
-
-    // Close the menu when a link is clicked (optional)
-    navLinks.querySelectorAll('a').forEach(link => {
-        link.addEventListener('click', function() {
-            navLinks.classList.remove('active');
-        });
+        navMenu.classList.toggle('active');
     });
 });
